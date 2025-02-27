d0f67355f8bb5c1c61a88212db71cf463284df5e
imnxtamess <davide.morisco.work@gmail.com>
2025-02-27 15:02:50 +0100

    fix bug of async code

--- main.js
+++ main.js
@@ -4,25 +4,27 @@
 
 const emailListEl = document.getElementById("email_list")
 
-// create global variables
-
-const randomEmailList = []
-
 // make an ajax request to the boolean endpoint and wrap it in a function
 
-function pushRandomEmail(randomEmailList) {
-  fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then(response => response.json())
-    .then(data => {
-      // save the random email in a variable 
-      const randomEmail = data.response
-      randomEmailList.push(randomEmail)
-    })
+function pushRandomEmail10times() {
+  for (let i = 0; i < 10; i++) {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then(response => response.json())
+      .then(data => {
+        // save the random email in a variable 
+        const randomEmail = data.response
+        // modify the html
+        console.log(randomEmail);
+        // create a new li node element
+        const liEl = document.createElement('li')
+        console.log(liEl);
+        // append the content to the list items
+        liEl.append(randomEmail)
+        // append the li child to the ul parent
+        emailListEl.appendChild(liEl)
+      })
+  }
 }
 
-for (let i = 0; i < 10; i++) {
-  pushRandomEmail(randomEmailList)
-}
-
-
+pushRandomEmail10times()
 
